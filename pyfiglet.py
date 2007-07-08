875f50e07b8099bc69b936c4fd6c265aae12d684
cjones <cjones@d4b09251-c226-dc11-aa29-00301bb40f23>
2007-07-08 20:34:32 +0000

    More useful defaults
    git-svn-id: https://pyfiglet.svn.sourceforge.net/svnroot/pyfiglet/trunk@5 d4b09251-c226-dc11-aa29-00301bb40f23

--- pyfiglet.py
+++ pyfiglet.py
@@ -143,17 +143,16 @@ class ZippedFigletFont(FigletFont):
 		FigletFont.__init__(self, dir=dir, font=font)
 
 	def readFontFile(self):
-		zipPath = '%s/%s' % (self.dir, self.zipfile)
-		if os.path.exists(zipPath) is False:
-			raise FontNotFound, "%s doesn't exist" % zipPath
+		if os.path.exists(self.zipfile) is False:
+			raise FontNotFound, "%s doesn't exist" % self.zipfile
 
 		fontPath = 'fonts/%s.flf' % self.font
 
 		try:
-			z = ZipFile(zipPath, 'r')
+			z = ZipFile(self.zipfile, 'r')
 			files = z.namelist()
 			if fontPath not in files:
-				raise FontNotFound, '%s not found in %s' % (self.font, zipPath)
+				raise FontNotFound, '%s not found in %s' % (self.font, self.zipfile)
 
 			self.data = z.read(fontPath)
 
@@ -164,13 +163,14 @@ class ZippedFigletFont(FigletFont):
 
 
 class Figlet(object):
-	def __init__(self, dir='.', font='standard', direction='auto', justify='auto', width=80, zipfile=None):
+	def __init__(self, dir=None, zipfile=None, font='standard', direction='auto', justify='auto', width=80):
 		self.dir = dir
 		self.font = font
 		self._direction = direction
 		self._justify = justify
 		self.width = width
 		self.zipfile = zipfile
+
 		self.setFont()
 
 	def setFont(self, **kwargs):
@@ -180,10 +180,24 @@ class Figlet(object):
 		if kwargs.has_key('font'):
 			self.font = kwargs['font']
 
-		if self.zipfile is None:
-			self.Font = FigletFont(dir=self.dir, font=self.font)
-		else:
-			self.Font = ZippedFigletFont(dir=self.dir, font=self.font, zipfile=self.zipfile)
+		if kwargs.has_key('zipfile'):
+			self.zipfile = kwargs['zipfile']
+
+
+		Font = None
+		if self.zipfile is not None:
+			try: Font = ZippedFigletFont(dir=self.dir, font=self.font, zipfile=self.zipfile)
+			except: pass
+
+		if Font is None and self.dir is not None:
+			try: Font = FigletFont(dir=self.dir, font=self.font)
+			except: pass
+
+		if Font is None:
+			raise FontNotFound, "Couldn't load font %s: Not found" % self.font
+
+		self.Font = Font
+
 
 	def getDirection(self):
 		if self._direction == 'auto':
@@ -240,13 +254,18 @@ class Figlet(object):
 
 
 def main():
+	dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
 	parser = OptionParser(version=__version__, usage='%prog [options] text..')
 
 	parser.add_option(	'-f', '--font', default='standard',
 				help='font to render with (default: %default)', metavar='FONT' )
 
-	parser.add_option(	'-d', '--fontdir', default='.',
-				help='location of font files (default: %default)', metavar='DIR' )
+	parser.add_option(	'-d', '--fontdir', default=None,
+				help='location of font files', metavar='DIR' )
+
+	parser.add_option(	'-z', '--zipfile', default=dir+'/fonts.zip',
+				help='specify a zipfile to use instead of a directory of fonts' )
 
 	parser.add_option(	'-D', '--direction', type='choice', choices=('auto', 'left-to-right', 'right-to-left'),
 				default='auto', metavar='DIRECTION',
@@ -259,8 +278,6 @@ def main():
 	parser.add_option(	'-w', '--width', type='int', default=80, metavar='COLS',
 				help='set terminal width for wrapping/justification (default: %default)' )
 
-	parser.add_option(	'-z', '--zipfile', default=None,
-				help='specify a zipfile to use instead of a directory of fonts' )
 
 
 	opts, args = parser.parse_args()
