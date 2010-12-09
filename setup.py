cb95e192beb5f1a0523465de06258731a8f6b8c6
Peter Waller <peter.waller@cern.ch>
2010-12-09 14:41:51 +0100

    Add setup.py (courtesy of Stefano Rivera) http://sourceforge.net/tracker/index.php?func=detail&aid=2933718&group_id=200820&atid=975074

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyfiglet',
+      version='0.4',
+      description='Pure-python FIGlet implementation',
+      author='Christopher Jones',
+      author_email='cjones@insub.org',
+      url='http://sourceforge.net/projects/pyfiglet/',
+      py_modules=['pyfiglet'],
+)
+
