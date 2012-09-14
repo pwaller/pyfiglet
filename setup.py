ad86643d782d5749d2891547c7bb47d5a182cab0
Peter Waller <peter.waller@gmail.com>
2012-09-14 11:08:43 +0100

    Version number bump => 0.6.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pyfiglet',
-      version='0.6.1dev',
+      version='0.6.1',
       description='Pure-python FIGlet implementation',
       author='Peter Waller (Thanks to Christopher Jones and Stefano Rivera)',
       author_email='peter.waller@gmail.com',
