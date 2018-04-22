7b8b201b391a22805fe3236629a1775724f2e156
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-04-22 12:20:16 +0200

    Erase blank lines surrounding output ; Fix color not being reset in 3.6

--- pyfiglet/__init__.py
+++ pyfiglet/__init__.py
@@ -46,20 +46,22 @@ COLOR_CODES = {'BLACK': 30, 'RED': 31, 'GREEN': 32, 'YELLOW': 33, 'BLUE': 34, 'M
 
 RESET_COLORS = b'\033[0m'
 
+
 def figlet_format(text, font=DEFAULT_FONT, **kwargs):
     fig = Figlet(font, **kwargs)
     return fig.renderText(text)
 
 
 def print_figlet(text, font=DEFAULT_FONT, colors=":", **kwargs):
-    ansiColors  = parse_color(colors)
+    ansiColors = parse_color(colors)
     if ansiColors:
-        print(ansiColors)
+        sys.stdout.write(ansiColors)
 
     print(figlet_format(text, font, **kwargs))
 
     if ansiColors:
-        print(RESET_COLORS)
+        sys.stdout.write(RESET_COLORS.decode('UTF-8', 'replace'))
+        sys.stdout.flush()
 
 
 class FigletError(Exception):
