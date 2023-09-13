pwaller/pyfiglet

commit 785f90a7f546f0e94a40ad0e9cc3e5e64322805d

Add support for zipped fonts

pyfiglet/__init__.py
@@ -131,18 +131,35 @@ class FigletFont(object):
         """
         Load font data if exist
         """
+        # Find a plausible looking font file.
+        data = None
+        f = None
         for extension in ('tlf', 'flf'):
             fn = '%s.%s' % (font, extension)
             if pkg_resources.resource_exists('pyfiglet.fonts', fn):
-                data = pkg_resources.resource_string('pyfiglet.fonts', fn)
-                data = data.decode('UTF-8', 'replace')
-                return data
+                f = pkg_resources.resource_stream('pyfiglet.fonts', fn)
+                break
             else:
                 for location in ("./", SHARED_DIRECTORY):
                     full_name = os.path.join(location, fn)
                     if os.path.isfile(full_name):
-                        with open(full_name, 'rb') as f:
-                            return f.read().decode('UTF-8', 'replace')
+                        f = open(full_name, 'rb')
+                        break
+
+        # Unzip the first file if this file/stream looks like a ZIP file.
+        if f:
+            if zipfile.is_zipfile(f):
+                with zipfile.ZipFile(f) as zip_file:
+                    zip_font = zip_file.open(zip_file.namelist()[0])
+                    data = zip_font.read()
+            else:
+                # ZIP file check moves the current file pointer - reset to start of file.
+                f.seek(0)
+                data = f.read()
+
+        # Return the decoded data (if any).
+        if data:
+            return data.decode('UTF-8', 'replace')
         else:
             raise FontNotFound(font)
 
@@ -158,8 +175,17 @@ class FigletFont(object):
             f = open(full_file, 'rb')
         else:
             f = pkg_resources.resource_stream('pyfiglet.fonts', font)
-        header = f.readline().decode('UTF-8', 'replace')
+
+        if zipfile.is_zipfile(f):
+            # If we have a match, the ZIP file spec says we should just read the first file in the ZIP.
+            with zipfile.ZipFile(f) as zip_file:
+                zip_font = zip_file.open(zip_file.namelist()[0])
+                header = zip_font.readline().decode('UTF-8', 'replace')
+        else:
+            header = f.readline().decode('UTF-8', 'replace')
+
         f.close()
+
         return cls.reMagicNumber.search(header)
 
     @classmethod

commit 6d408fc6653630198b3a4f04075e1a93d64ef420

Close file handle in preloadFont
Commit 785f90a7f546f0e94a40ad0e9cc3e5e64322805d introduced a regression
by not using a context manager when opening the file, which eventually
lead to a resource leak because the file handle 'f' never gets closed.

This is fixed by passing the font path, instead of the open file handle,
and by opening the handle using a 'with' context manager.

The issue can be reproduced by running pytest with the '-W error' flag
for the following minimal example:

    def test_file_gets_closed():
        Figlet().renderText("")

This will succeed now, but fails for v1.0.1.

pyfiglet/__init__.py
@@ -135,30 +135,31 @@ class FigletFont(object):
         """
         # Find a plausible looking font file.
         data = None
-        f = None
+        font_path = None
         for extension in ('tlf', 'flf'):
             fn = '%s.%s' % (font, extension)
             path = importlib.resources.files('pyfiglet.fonts').joinpath(fn)
             if path.exists():
-                f = path.open('rb')
+                font_path = path
                 break
             else:
                 for location in ("./", SHARED_DIRECTORY):
                     full_name = os.path.join(location, fn)
                     if os.path.isfile(full_name):
-                        f = open(full_name, 'rb')
+                        font_path = pathlib.Path(full_name)
                         break
 
         # Unzip the first file if this file/stream looks like a ZIP file.
-        if f:
-            if zipfile.is_zipfile(f):
-                with zipfile.ZipFile(f) as zip_file:
-                    zip_font = zip_file.open(zip_file.namelist()[0])
-                    data = zip_font.read()
-            else:
-                # ZIP file check moves the current file pointer - reset to start of file.
-                f.seek(0)
-                data = f.read()
+        if font_path:
+            with font_path.open('rb') as f:
+                if zipfile.is_zipfile(f):
+                    with zipfile.ZipFile(f) as zip_file:
+                        zip_font = zip_file.open(zip_file.namelist()[0])
+                        data = zip_font.read()
+                else:
+                    # ZIP file check moves the current file pointer - reset to start of file.
+                    f.seek(0)
+                    data = f.read()
 
         # Return the decoded data (if any).
         if data: